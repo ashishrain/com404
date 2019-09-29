7e807885b3dcc8d757eedb752f6f0d1ebb753d18
ashishraiworld <ashishraiworld@gmail.com>
2019-09-29 15:36:24 +0000

    Added code to demonstrate the use of string operators.

---
+++ 1-basics/2-input/4-string-operators/bot.py
@@ -0,0 +1,18 @@
+#reads the numbers of lives and stores into variable lives
+print("Please enter the number of lives.")
+lives = int(input())
+
+#reads the number of energy level and stores into variable energy
+print("Please enter the energy level.")
+energy = int(input())
+
+#reads the number of shield level and stores into variable shield
+print("Please enter the shield level.")
+shield = int(input())
+
+print("Health has been set. \n")
+
+#displays lives, energy level and shield
+print("Lives: " + ("♥" * lives) )
+print("Energy: " + ("●" * energy) )
+print("Shield: " + ("♦" * shield) )
