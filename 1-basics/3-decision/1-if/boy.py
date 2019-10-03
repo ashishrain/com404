930e37c8f8fc985177bb2d5a0b0eca6e076b5f79
ashishraiworld <ashishraiworld@gmail.com>
2019-10-03 08:21:21 +0000

    Added code to demonstrate the use of an if statement.

---
+++ 1-basics/3-decision/1-if/boy.py
@@ -0,0 +1,9 @@
+#code to demonstrate the use of an if statement
+
+#asking what type of book is this and storing it in book variable
+print("What type of book is this")
+book = str(input())
+if (book == "adventure"):
+    print("I like adventure books!")
+
+print("Finished reading book.")
