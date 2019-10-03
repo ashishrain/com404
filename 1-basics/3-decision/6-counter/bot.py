34d12fce50d022c0ef3fe15b060e875373ad6de2
ashishraiworld <ashishraiworld@gmail.com>
2019-10-03 09:33:33 +0000

    Added code to demonstrate how a variable can be used as a counter.

---
+++ 1-basics/3-decision/6-counter/bot.py
@@ -0,0 +1,33 @@
+#code to demonstrate how a variable can be used as a counter
+
+#created counters for both even and odd which is set to 0 as there is no odd numbers right now
+even_counter = 0
+odd_counter = 0
+
+#asks users to enter a whole number
+print("Please enter the first whole number.")
+first_number = int(input())
+
+#checks if it is even or odd and adds 1 to relevant counters/variable
+if (first_number % 2 != 0):
+    odd_counter += 1
+else:
+    even_counter += 1
+
+print("Please enter the second whole number.")
+second_number = int(input())
+
+if (second_number % 2 != 0):
+    odd_counter += 1
+else:
+    even_counter += 1
+
+print("Please enter the third whole number.")
+third_number = int(input())
+
+if (third_number % 2 != 0):
+    odd_counter = odd_counter + 1
+else:
+    even_counter += 1
+
+print("There were " + str(even_counter) + " even and " + str(odd_counter) + " odd numbers.")
