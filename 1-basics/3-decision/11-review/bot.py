005d7643cc443199192a3f0f06a391668ef94a18
ashishraiworld <ashishraiworld@gmail.com>
2019-10-03 11:36:43 +0000

    game

---
+++ 1-basics/3-decision/11-review/bot.py
@@ -0,0 +1,8 @@
+#code to shoot the ball and score against the ai
+
+#explain the game to the users
+print("Welcome to this game. This is a Football game where you will have the ball and go on the way to score.")
+
+#ask user whether to dribble and shoot the ball or dribble and pass the ball
+print("You will have to choose and perform 3 things. Do you want to dribble, pass or shoot?")
+decision = input()
