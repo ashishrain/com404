d9b6111d636e1e46ef834e94175d263a2568e411
ashishraiworld <ashishraiworld@gmail.com>
2019-10-18 11:45:23 +0000

    Added code to use the conditional statements.

---
+++ 1-basics/6-practice/2-decision/bot.py
@@ -0,0 +1,22 @@
+#code to help Dr Frankenstein bring his monster alive
+
+#asking user what type of night it be
+print("What type of night be it tonight")
+
+#storing the input to night_type variable
+night_type = input()
+
+#using if conditions
+#if the user input is "lightening" do:
+if (night_type == "lightening"):
+    print("It is time to be ALIVE!")
+
+#if the user input is "cloudy" do:
+elif(night_type == "cloudy"):
+    print("The sky is stirring but we must wait.")
+
+#if the user input is other
+else:
+    print("Soon my precious. Soon.")
+
+
