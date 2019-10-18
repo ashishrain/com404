2b9f0241d1e442d354e17f403e234a6d493c9cd4
ashishraiworld <ashishraiworld@gmail.com>
2019-10-18 11:44:42 +0000

    Added code to display a message.

---
+++ 1-basics/6-practice/1-input-output/bot.py
@@ -0,0 +1,8 @@
+#code to welcome guests
+
+#askig trick or treat and getting user response
+print("Trick or Treat?")
+response = input()
+
+#display it will be a ghostly followed by the response
+print("It will be ghostly " + response)
