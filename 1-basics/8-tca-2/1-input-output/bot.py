41c524bca612c811387b1f892fec46263857d1c5
ashishraiworld <ashishraiworld@gmail.com>
2019-10-25 08:58:41 +0000

    added code to demonstrate taking user input and output it

---
+++ 1-basics/8-tca-2/1-input-output/bot.py
@@ -0,0 +1,8 @@
+#code to demonstrate taking user input and output it
+
+#ask user for response
+print("What happens when the last petal falls?")
+response = input()
+
+#display the output using the user input
+print("My dear Bella when the last petal falls " + response)
