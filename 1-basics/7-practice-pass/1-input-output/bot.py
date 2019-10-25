2514821cef1ad6784c26362c6cf76da858c1413f
ashishraiworld <ashishraiworld@gmail.com>
2019-10-25 08:28:04 +0000

    code to greet the user

---
+++ 1-basics/7-practice-pass/1-input-output/bot.py
@@ -0,0 +1,10 @@
+#code to greet the user
+
+#ask user for the name
+print("By what name are you known?")
+
+#read user response
+name = input()
+
+#display the message
+print("Live long and prosper " + name)
