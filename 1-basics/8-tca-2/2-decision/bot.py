952d7aed83691ff6bfa95b6c56d10721d5a60f43
ashishraiworld <ashishraiworld@gmail.com>
2019-10-25 09:03:04 +0000

    added code to demonstrate decision making

---
+++ 1-basics/8-tca-2/2-decision/bot.py
@@ -0,0 +1,11 @@
+#code to demonstrate decision making
+
+#ask for user input
+print("Have you fear in your heart?")
+response = input()
+
+#use of if statement for decision making
+if (response == "yes"):
+    print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
+else:
+    print("The force is strong in you. You may be a Jedi apprentice.")
