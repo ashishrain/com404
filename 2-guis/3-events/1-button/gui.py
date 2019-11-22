5cec5dba74b68a75a19233d6d5cd41eed41b1969
ashishraiworld <ashishraiworld@gmail.com>
2019-11-22 10:46:20 +0000

    Added code to display a messagebox on user click.

--- 2-guis/3-events/1-button/gui.py
+++ 2-guis/3-events/1-button/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Gui(Tk):
     
@@ -15,6 +16,7 @@ class Gui(Tk):
         self.__add_buy_button()
         
     def __add_heading_label(self):
+        #create
         self.heading_label = Label()
         self.heading_label.grid(row=0,
                                 column=0,
@@ -25,6 +27,7 @@ class Gui(Tk):
                                            font="#Ariel 25")
         
     def __add_instruction_label(self):
+        #create
         self.instruction_label = Label()
         self.instruction_label.grid(row=1,
                                     column=0,
@@ -36,6 +39,7 @@ class Gui(Tk):
                                                fg="#000110")
         
     def __add_tickets_entry(self):
+        #create
         self.tickets_entry = Entry()
         self.tickets_entry.grid(row=2,
                                 column=0,
@@ -44,11 +48,28 @@ class Gui(Tk):
         #self.__add_tickets_entry.configure()
         
     def __add_buy_button(self):
+        #create
         self.buy_button = Button()
         self.buy_button.grid(row=3,
                             column=0,
                             pady=20)
         #style
-        self.buy_button.configure(text="Buy",
+        self.buy_button.configure(text="Submit",
                                         font="Ariel 16",
                                         fg="#000110")
+        #events
+        self.buy_button.bind("<ButtonRelease-1>", self.__buy_button_clicked)
+
+    def __buy_button_clicked(self, event):
+        #get the amount from self.tickets_entry of __add_tickets_entry()
+        no_of_tickets =  int(self.tickets_entry.get())
+
+        #display using  if statement
+        if (no_of_tickets == 1):
+            messagebox.showinfo("Purchased!", "You have purchased 1 ticket!")
+        elif (no_of_tickets > 1):
+            messagebox.showinfo("Purchased!", "You have purchased " + str(no_of_tickets) + " tickets!")
+        else:
+            messagebox.showinfo("Error!", "Invalid amount. Please try again.")        
+
+        
