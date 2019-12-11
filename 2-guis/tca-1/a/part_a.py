ab8bef6a6809c5f6929ba5b8d80c355a0729adda
ashishraiworld <ashishraiworld@gmail.com>
2019-12-11 14:29:25 +0000

    Added the code for part a2 and a3

--- 2-guis/tca-1/a/part_a.py
+++ 2-guis/tca-1/a/part_a.py
@@ -8,6 +8,9 @@ class Gui(Tk):
     def __init__(self):
         super().__init__()
 
+        #load resources
+        self.default_image = PhotoImage(file="default.gif")
+
         #set window attributes
         self.title("Newsletter")
         self.configure(bg="#ccc",
@@ -21,6 +24,7 @@ class Gui(Tk):
         self.__add_subheading_label()
         self.__add_email_label()
         self.__add_email_entry()
+        self.__add_default_image_label()
         self.__add_subscribe_button()
 
     def __add_outer_frame(self):
@@ -33,7 +37,7 @@ class Gui(Tk):
         self.heading_label = Label(self.outer_frame)
         self.heading_label.grid(row=0,
                                 column=0,
-                                columnspan=2,
+                                columnspan=3,
                                 pady=10) #is this the right way to put padding top and bottom?
         self.heading_label.configure(text="RECEIVE OUR NEWSLETTER",
                                      font="Arial 14")
@@ -42,7 +46,7 @@ class Gui(Tk):
         self.subheading_label = Label(self.outer_frame)
         self.subheading_label.grid(row=1,
                                    column=0,
-                                   columnspan=2,
+                                   columnspan=3,
                                    pady=10,
                                    padx=10) #is this the right way to put padding all the way round?
         self.subheading_label.configure(text="Please enter your email below to receive our newsletter.",
@@ -68,16 +72,31 @@ class Gui(Tk):
                                    width=30,
                                    border=2)
 
+    def __add_default_image_label(self):
+        self.default_image_label = Label(self.outer_frame)
+        self.default_image_label.grid(row=2,
+                                      column=2,
+                                      sticky=W,
+                                      padx=10)
+        self.default_image_label.configure(image=self.default_image,
+                                           height=15,
+                                           width=15)
+
     def __add_subscribe_button(self):
         self.subscribe_button = Button(self.outer_frame)
         self.subscribe_button.grid(row=3,
                                    column=0,
-                                   columnspan=2,
+                                   columnspan=3,
                                    sticky=N+E+S+W,
                                    pady=10)
         self.subscribe_button.configure(text="Subscribe",
                                         font="Arial 10",
                                         bg="#fee")
+        #events
+        self.subscribe_button.bind("<ButtonRelease-1>", self.__subscribe_button_clicked)
+
+    def __subscribe_button_clicked(self, event):
+        messagebox.showinfo("Newsletter", "Subscribed!")
 
 # Create an object of the Gui class when this module is executed
 if (__name__ == "__main__"):
